440ce508e741b84373dae503dffee909600a6075
Matej Strnad <matej.strnad.97@gmail.com>
2020-03-08 15:20:18 +0100

    render basic map with mock data

--- pages/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Index = function () { return <p>Ahoj!</p>; };
-exports["default"] = Index;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,27 @@
-const Index = () => <p>Ahoj!</p>
+import { ComposableMap, Geographies, Geography } from 'react-simple-maps'
+import styled from 'styled-components'
 
-export default Index
+const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json'
+
+export default () => (
+  <div>
+    <ComposableMap>
+      <Geographies geography={geoUrl}>
+        {({ geographies }) => geographies.map(renderGeography)}
+      </Geographies>
+    </ComposableMap>
+  </div>
+)
+
+const renderGeography = geo => {
+  console.log(geo.properties)
+  return (
+    <StyledGeography key={geo.rsmKey} geography={geo} opacity={Math.random()} />
+  )
+}
+
+const StyledGeography = styled(Geography)<{ opacity: number }>`
+  fill: red;
+  opacity: ${props => props.opacity};
+`
