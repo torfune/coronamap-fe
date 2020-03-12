f9235431d0060569151a1ce6e6995c58fa9cd3ee
Matej Strnad <matej.strnad.97@gmail.com>
2020-03-12 13:21:59 +0100

    Update data

--- components/CasesMapWorld.tsx
+++ components/CasesMapWorld.tsx
@@ -2,7 +2,18 @@ import SectionLabel from './SectionLabel'
 import MapChart from './MapChart'
 import { SOURCE } from '../constants'
 import createDate from '../utils/createDate'
-import data from '../public/data/world/world-cases-11-03-20.json'
+import worldData from '../public/data/world/world-cases-11-03-20.json'
+import euData from '../public/data/eu/eu-cases-12-03-20_08-00.json'
+import Data from '../types/Data'
+
+const data: Data = { ...worldData }
+const entries = Object.entries(euData)
+for (let i = 0; i < entries.length; i++) {
+  const [code, value] = entries[i]
+  if (!data[code] || value > data[code]) {
+    data[code] = value
+  }
+}
 
 const CasesMapWorld = () => (
   <>
@@ -11,7 +22,7 @@ const CasesMapWorld = () => (
       type="WORLD"
       data={data}
       source={SOURCE.WHO}
-      updatedAt={createDate('11-03-20')}
+      updatedAt={createDate('12-03-20')}
     />
   </>
 )
