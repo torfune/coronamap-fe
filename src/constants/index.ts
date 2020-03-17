3ede90b148b2885d9e748331760e35bd90d11864
Matej Strnad <matej.strnad.97@gmail.com>
2020-03-17 10:47:02 +0100

    update data

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export const UPDATED_AT = {
-  USA: '15-03-20',
-  WORLD: '15-03-20',
-  EUROPE: '15-03-20',
+  USA: '16-03-20',
+  WORLD: '16-03-20',
+  EUROPE: '16-03-20',
 }
 
 export const COLOR = {
